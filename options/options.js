e9a0de6d6b23693ce9075f8837551a430a5d4495
Crowdbotics <team@crowdbotics.com>
2021-08-09 21:13:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "enyo_29675Identifier",
+  url: "https://enyo_29675Identifier.botics.co",
+  api: "https://enyo_29675Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
